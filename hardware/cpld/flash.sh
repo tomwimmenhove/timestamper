bc0cca62f19bcece2154bcaf4e67bc9020930350
tom.wimmenhove@gmail.com <tom.wimmenhove@gmail.com>
2019-03-20 18:59:09 -0400

    Using FTDI FT232H JTAG programmer

--- hardware/cpld/flash.sh
+++ hardware/cpld/flash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-scp main.jed pi@jtagpi:~ && time ssh pi@jtagpi -- sudo xc3sprog -c sysfsgpio main.jed
+#scp main.jed pi@jtagpi:~ && time ssh pi@jtagpi -- sudo xc3sprog -c sysfsgpio main.jed
+xc3sprog -c ft232h main.jed
+
 
